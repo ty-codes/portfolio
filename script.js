a8f6b18e7c45125c6d6e579e1bcd0debd85ff3d6
Bolatito Adeoye <titoadeoye00@gmail.com>
2022-09-29 03:17:50 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import projects from './projects.json'assert {type: 'json'};
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -21,4 +22,13 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
 	e.target.elements.email.value = '';
 	e.target.elements.message.value = '';
 });
+
+// project data
+const projectNumber = projects.projects.length;
+const projectList = document.getElementById('project_list')
+for(let i=0; i<projectNumber; i++) {
+    projectList.innerHTML = projectList.innerHTML + "<div class='project-tile'><span><img src=" + projects.projects[i].image+ " alt=" + projects.projects[i].name+ "></span>" +
+    "<h2 class='title'><a target='_blank' href=" + projects.projects[i].link+ ">" + projects.projects[i].name+ "</a></h2>" +
+    "<p class='stack'>" + projects.projects[i].stack+ "</p></div>"
+}
 
